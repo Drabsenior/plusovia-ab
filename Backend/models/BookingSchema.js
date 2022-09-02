43826e372b0c522f7a9051a62d524330969ffb97
Drabsenior <drabsenior@gmail.com>
2022-09-02 16:21:33 +0300

    timestamp

--- Backend/models/BookingSchema.js
+++ Backend/models/BookingSchema.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
-const BookingSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
+const BookingSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    room: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
   },
-  room: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 const books = mongoose.model("Booking", BookingSchema);
-module.exports= books;
+module.exports = books;
